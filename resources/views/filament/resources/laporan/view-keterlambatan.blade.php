451074854879ac2bf14d2e4aec7454fe50abfa04
KekeNihBoss <keishaoktavian41@gmail.com>
2024-12-03 12:16:27 +0700

    View Details V3

--- resources/views/filament/resources/laporan/view-keterlambatan.blade.php
+++ resources/views/filament/resources/laporan/view-keterlambatan.blade.php
@@ -3,15 +3,19 @@
     <table class="table-auto w-full mt-4">
         <thead>
             <tr>
-                <th class="px-4 py-2 border">Nama Siswa</th>
+                <th class="px-4 py-2 border">NIS</th>
+                <th class="px-4 py-2 border">Nama</th>
+                <th class="px-4 py-2 border">Kelas</th>
                 <th class="px-4 py-2 border">Keterangan</th>
             </tr>
         </thead>
         <tbody>
             @forelse ($keterlambatan as $data)
                 <tr>
+                    <td class="px-4 py-2 border">{{ $data->siswa->nis ?? 'Tidak Diketahui' }}</td>
                     <td class="px-4 py-2 border">{{ $data->siswa->nama ?? 'Tidak Diketahui' }}</td>
-                    <td class="px-4 py-2 border">{{ $data->keterangan ?? 'Tidak ada keterangan' }}</td>
+                    <td class="px-4 py-2 border">{{ $data->siswa->kelas ?? 'Tidak Diketahui' }}</td>
+                    <td class="px-4 py-2 border">{{ $data->alasan ?? 'Tidak ada keterangan' }}</td>
                 </tr>
             @empty
                 <tr>
